e48d7edc9fdacda2dc273aadcf4e15ebdbb55f48
Mayo <mayolito.waje08@gmail.com>
2022-10-31 21:16:37 +0800

    set buttons

--- script.js
+++ script.js
@@ -26,3 +26,47 @@ function operate(num1, num2, operator) {
       return divide(num1, num2);
   }
 }
+
+function getClassNameBySymbol(symbol) {
+  switch (symbol) {
+    case 'AC': return 'reset';
+    case 'C': return 'delete';
+    case '%': return 'percent';
+    case '÷': return 'divide';
+    case 'x': return 'multiply';
+    case '-': return 'subtract';
+    case '+': return 'add';
+    case '.': return 'decimal';
+    case '=': return 'equal';
+  }
+}
+
+function createNumButtons() {
+  for (let numKey = 0; numKey <= 9; numKey++) {
+    const numButton = document.createElement("button");
+    numButton.classList.add("number");
+    numButton.dataset.key = numKey;
+    numButton.innerText = numKey;
+    document.getElementById("buttons").appendChild(numButton);
+  }
+}
+
+function createSymbolButtons() {
+  const symbols = ['AC', 'C', '%', '÷', 'x', '-', '+', '.', '='];
+  symbols.forEach(symbol => {
+    const symbolButton = document.createElement("button");
+    symbolButton.classList.add(getClassNameBySymbol(symbol));
+    symbolButton.dataset.key = symbol;
+    symbolButton.innerText = symbol;
+    document.getElementById("buttons").appendChild(symbolButton);
+  });
+}
+
+function createButtons() {
+  createNumButtons();
+  createSymbolButtons();
+}
+
+window.addEventListener("DOMContentLoaded", event => {
+  createButtons();
+})
