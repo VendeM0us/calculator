c4a9d43ff1ae8352794460a5467988ea03eec86f
Mayo <mayolito.waje08@gmail.com>
2022-11-01 12:07:30 +0800

    add function to convert expression to postfix order and compute the result

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function compute(expression) {
+  const postfixOrder = translateToPostfixOrder(expression);
+  return computeFromPostfixOrder(postfixOrder);
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -27,12 +32,82 @@ function operate(num1, num2, operator) {
   }
 }
 
+function computeFromPostfixOrder(postfixOrder) {
+  const stack = [];
+
+  for (let i = 0; i < postfixOrder.length; i++) {
+    const ele = postfixOrder[i];
+
+    if (isOperand(ele)) {
+      stack.push(toNumber(ele));
+    } else {
+      const num2 = stack.pop();
+      const num1 = stack.pop();
+      const result = operate(num1, num2, ele);
+      const precise = result.toPrecision(12);
+      stack.push(Number(precise));
+    }
+  }
+
+  return stack[0];
+}
+
+function toNumber(str) {
+  if (str.includes("%")) {
+    const toDecimal = Number(str.slice(0, str.length - 1)) * 0.01;
+    const precise = toDecimal.toPrecision(12);
+    return Number(precise);
+  } else if (parseInt(str) === Number(str)) {
+    return Number(str);
+  } else {
+    const precise = parseFloat(str).toPrecision(12);
+    return Number(precise);
+  }
+}
+
+function precedence(operator) {
+  switch (operator) {
+    case '*': 
+    case '/': return 2;
+    case '+':
+    case '-': return 1;
+  }
+}
+
+function isOperand(ele) {
+  const operators = "*/+-";
+  return !operators.includes(ele);
+}
+
+function translateToPostfixOrder(expression) {
+  const parts = expression.split(" ");
+  const stack = [];
+  const postfixOrder = [];
+
+  for (let i = 0; i < parts.length; i++) {
+    const ele = parts[i];
+
+    if (isOperand(ele)) {
+      postfixOrder.push(ele);
+    } else {
+      while (stack.length !== 0 && precedence(ele) <= precedence(stack[stack.length - 1])) {
+        postfixOrder.push(stack.pop());
+      }
+      stack.push(ele);
+    }
+  }
+
+  while(stack.length !== 0) postfixOrder.push(stack.pop());
+
+  return postfixOrder;
+}
+
 function getClassNameBySymbol(symbol) {
   switch (symbol) {
     case 'AC': return 'reset';
     case 'C': return 'delete';
     case '%': return 'percent';
-    case '÷': return 'divide';
+    case '/': return 'divide';
     case 'x': return 'multiply';
     case '-': return 'subtract';
     case '+': return 'add';
@@ -52,12 +127,12 @@ function createNumButtons() {
 }
 
 function createSymbolButtons() {
-  const symbols = ['AC', 'C', '%', '÷', 'x', '-', '+', '.', '='];
+  const symbols = ['AC', 'C', '%', '/', 'x', '-', '+', '.', '='];
   symbols.forEach(symbol => {
     const symbolButton = document.createElement("button");
     symbolButton.classList.add(getClassNameBySymbol(symbol));
     symbolButton.dataset.key = symbol;
-    symbolButton.innerText = symbol;
+    symbol === '/' ? symbolButton.innerText = '÷' : symbolButton.innerText = symbol;
     document.getElementById("buttons").appendChild(symbolButton);
   });
 }
